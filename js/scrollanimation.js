7bca4318d63605e2e2b496feff8871cbf88ab9e7
Anurag Mathews <anuragmathews619@gmail.com>
2019-06-07 21:14:02 +0530

    adding correct pages

--- js/scrollanimation.js
+++
@@ -1,5 +0,0 @@
-ScrollReveal().reveal('#box1');
-ScrollReveal().reveal('#box2');
-ScrollReveal().reveal('#box3');
-ScrollReveal().reveal('#main .razorpay-payment-button', {delay: 500});
-ScrollReveal().reveal('#note', {delay: 700});
